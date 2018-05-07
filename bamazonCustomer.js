321b4be4d8b9d6e6864f488c2860914a3b71173e
carina cardona <carina.cardona89@gmail.com>
2018-05-07 14:46:04 -0400

    finished code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,76 +1,83 @@
 /*
 the brain of the database
-
 */
-
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+  host: "localhost",
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "password",
-    database: "bamazon_DB"
-});
+  // Your password
+  password: " ",
+  database: "bamazon_DB"
+})
 
 //----------------------------------------------------------------------------------------------------------
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connection successful!");
+  // run the start function after the connection is made to prompt the use
+  makeTable(); //collects data from database to be printed onto screen
+})
 
-
-/*
-Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-*/
-
-//---------------------------------------------------------------------------------
-
-/*
-The app should then prompt users with two messages.
-
-The first should ask them the ID of the product they would like to buy.
-The second message should ask how many units of the product they would like to buy.
-*/
-
-// function which prompts the user for what action they should take
-function start() {
-  inquirer
-      .prompt({
-          name: "postOrBid",
-          type: "rawlist",
-          message: "Would you like to [POST] an auction or [BID] on an auction?",
-          choices: ["POST", "BID"]
-      })
-      .then(function(answer) {
-          // based on their answer, either call the bid or the post functions
-          if (answer.postOrBid.toUpperCase() === "POST") {
-              postAuction();
-          } else {
-              bidAuction();
-          }
-      });
+var makeTable = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + "||" + res[i].product_name + "||" +
+        res[i].dept_name + "||" + res[i].price + "||" + res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  })
 }
-
-
-//--------------------------------------------------------------------------------------------------------
-
-/*
-Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-However, if your store does have enough of the product, you should fulfill the customer's order.
-
-This means updating the SQL database to reflect the remaining quantity.
-Once the update goes through, show the customer the total cost of their purchase.
-*/
+var promptCustomer = function (res) { //take in reponse option
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "What would you like to buy? [Quit with Q]"
+      }]).then(function(answer){
+          var correct = false;
+if(answer.choice.toUpperCase()=="Q"){
+  process.exit();
+}
+          for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == answer.choice) {
+              correct = true;
+              var product = answer.choice;
+              var id = i;
+              inquier.prompt({
+                type: "input",
+                name: "quant",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                  if (isNaN(value) == false) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                }
+              }).then(function (answer) {
+                  if ((res[id].stock_quantity - answer.quant) > 0) {
+                    connection.query("UPDATE products SET stock_quantity=' " + (res[id].stock_quantity - answer.quant) + " ' WHERE product_name= '" + product + " ' ", function (err, res2) {
+                      console.log("Product bought!");
+                      makeTable();
+                    })
+                  }else{
+                    console.log("Not a valid selection!");
+                    promptCustomer(res);
+                  }
+                })
+              }
+          }
+          if(i==res.length && correct==false){
+            console.log("Not a valid selection!");
+            promptCustomer(res);
+          }
+        })
+      } 
